46e6cb605985c9795921c7bda67d16149bbe2f86
Hadar Dotan <hadard@wix.com>
2020-11-25 18:31:49 +0200

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sketch from "./Sketch"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sketch />
+    </div>
+  );
+}
+
+export default App;
